702caa8c5b2ee2fa5117c70abb3393731fb8445c
Louis Garman <louisgarman@gmail.com>
2011-02-28 11:57:45 +0000

    Recipe for some configs, gems and homebrew packages

---
+++ cookbooks/mac/attributes/mac.rb
@@ -0,0 +1,7 @@
+default[:gemrepos] = %w[http://gemcutter.org http://gems.github.com]
+default[:gems] = %w[capistrano capistrano-ext Nabaztag aws-s3]
+default[:brews] = %w[s3cmd htop]
+default[:aws][:access_key] = nil
+default[:aws][:secret_key] = nil
+default[:nabaztag][:mac] = nil
+default[:nabaztag][:token] = nil
---
+++ cookbooks/mac/recipes/default.rb
@@ -0,0 +1,54 @@
+include_recipe "chef-homebrew"
+
+homedir = "/Users/#{node[:current_user]}"
+
+directory "/usr/local" do
+  owner     node[:current_user]
+  group     "wheel"
+  mode      "0755"
+end
+
+template "#{homedir}/.gemrc" do
+  source    "_gemrc.erb"
+  owner     node[:current_user]
+  group     "staff"
+  mode      "0644"
+end
+
+link "/usr/local/bin/gitx" do
+  to "/Applications/GitX.app/Contents/Resources/gitx"
+  only_if "test -x /Applications/GitX.app/Contents/Resources/gitx"
+end
+
+node[:gems].each {|p| gem_package p }
+node[:brews].each {|p| package p }
+
+file "/usr/local/Cellar/htop/HEAD/bin/htop" do
+  owner     "root"
+  group     "staff"
+  mode      "6555"
+end
+
+template "#{homedir}/.nabaztag" do
+  source    "_nabaztag.erb"
+  owner     node[:current_user]
+  group     "staff"
+  mode      "0644"
+  not_if   { node[:nabaztag][:mac].nil? || node[:nabaztag][:token].nil? }
+end
+
+cookbook_file "/usr/local/bin/keychain" do
+  source    "keychain"
+  owner     node[:current_user]
+  group     "staff"
+  mode      "0755"
+end
+
+template "#{homedir}/.s3cfg" do
+  owner     node[:current_user]
+  group     "staff"
+  mode      "0600"
+  source    "_s3cfg.erb"
+  not_if   { node[:aws][:access_key].nil? || node[:aws][:secret_key].nil? }
+end
+
